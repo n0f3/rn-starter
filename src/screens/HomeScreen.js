e79c65058a6e2bf58d382bf5f070dbb184b3ac4c
Alessandro Metta <n0f3@outlook.com>
2020-04-15 18:24:59 -0400

    Finished section 4

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Test</Text>;
+const HomeScreen = ({
+  navigation: {
+    navigate
+  }
+}) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        title='Go to Components Demo'
+        onPress={() => {
+          navigate('Components')
+        }}
+      />
+      <Button
+        title='Go to List Demo'
+        onPress={() => {
+          navigate('List');
+        }}
+      />
+      {/* <TouchableOpacity onPress={() => {
+        props.navigation.navigate('List');
+      }}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
